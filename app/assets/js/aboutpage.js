728a8ba736024da2893815d5c96a5f1cb97cbca3
samodurov <samodirov.ivan@gmail.com>
2018-12-10 23:07:50 +0300

    update trust-us js

--- app/assets/js/aboutpage.js
+++ app/assets/js/aboutpage.js
@@ -15,12 +15,15 @@ var trustUsFeedback = function(){
     this.items = Array.from(document.querySelectorAll('.trust-us__icons-item'));
     this.itemsWrapper = document.getElementsByClassName('trust-us__icons')[0];
     this.round = document.getElementById('js-logo-wrapper');
+    this.backfeeds = Array.from(document.querySelectorAll('.audio-backfeed'));
     
-    this.moveRound(this.items[0]);
+    this.moveRound(this.items[0]);  
+    this.changeSlide(0);
 
     this.items.map((item,index) => {
         item.onclick = () => {            
             this.moveRound(item);
+            this.changeSlide(index);
         }
     })
 }
@@ -28,15 +31,24 @@ var trustUsFeedback = function(){
 trustUsFeedback.prototype.moveRound = function(element){
     var logoRect = element.getBoundingClientRect();
     var containerRect = this.itemsWrapper.getBoundingClientRect();
-
     var pos = {
         left: logoRect.left - containerRect.left,
         top: logoRect.top - containerRect.top
     }    
-
     this.round.style.left = pos.left + 'px';
     this.round.style.top = pos.top + 'px';    
 }
+
+trustUsFeedback.prototype.changeSlide = function(item_index) {
+    this.backfeeds.map((item, index) => {        
+        if(item_index === index) {
+            item.classList.remove('is_hidden');
+        } else {
+            item.classList.add('is_hidden');
+        }
+    })
+}
+
 
 
 
