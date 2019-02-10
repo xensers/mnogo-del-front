ac2837235ce4261a12406174bd597a3946e49f21
Eugene Naumov <xensers@gmail.com>
2019-02-10 20:38:53 +0400

    Начал делать перелистывание на предыдущий кейс

--- app/assets/js/case-detailed-page.js
+++ app/assets/js/case-detailed-page.js
@@ -1,21 +1,33 @@
 var elemSideRight;
+var elemSideLeft;
 var elemActiveStiker;
 var elemNextStiker;
+var elemPrevStiker;
 var elemArticle;
 var linkNextPage;
 var documentNextPage;
+var documentPrevPage;
 var timerClearStyle;
 
 function loadCaseDetailedPage() {
   elemSideRight    = document.querySelector('.layout__side_right');
+  elemSideLeft     = document.querySelector('.layout__side_left');
   elemActiveStiker = document.querySelector('.case-detailed__stiker');
   elemNextStiker   = document.querySelector('.case-detailed__next');
+  elemPrevStiker   = document.querySelector('.case-detailed__prev');
   elemArticle      = document.querySelector('.case-detailed__article');
   linkNextPage     = document.querySelector('.case-detailed__next a');
+  linkPrevPage     = document.querySelector('.case-detailed__prev a');
 
   getDocument(linkNextPage.href, function(doc) {
     documentNextPage = doc;
     elemSideRight.onclick = nextPage;
+    document.documentElement.classList.add('next-doc-ready');
+  });
+
+  getDocument(linkPrevPage.href, function(doc) {
+    documentPrevPage = doc;
+    elemSideLeft.onclick = prevPage;
   });
 
   elemSideRight.onmouseover = function() {
@@ -25,6 +37,15 @@ function loadCaseDetailedPage() {
   elemSideRight.onmouseout = function() {
     elemNextStiker.querySelector('.stiker').style.transform = '';
   }
+
+  elemSideLeft.onmouseover = function() {
+    elemPrevStiker.querySelector('.stiker').style.transform = 'translateX(5rem)';
+  }
+
+  elemSideLeft.onmouseout = function() {
+    elemPrevStiker.querySelector('.stiker').style.transform = '';
+  }
+
 }
 
 function nextPage(event)
@@ -34,15 +55,27 @@ function nextPage(event)
 
   var coordsActiveStiker = elemActiveStiker.getBoundingClientRect();
   var coordsNextStiker   = elemNextStiker.getBoundingClientRect();
+  var coordsPrevStiker   = elemPrevStiker.getBoundingClientRect();
 
   elemNextStiker.style.transform  = 'translateX('+ coordsNextStiker.x +'px) translateY('+ coordsNextStiker.y +'px)';
   elemNextStiker.style.top        = 0;
   elemNextStiker.style.left       = 0;
 
+  elemActiveStiker.style.transform  = 'translateX('+ coordsActiveStiker.x +'px) translateY('+ coordsActiveStiker.y +'px)';
+  elemActiveStiker.style.top        = 0;
+  elemActiveStiker.style.left       = 0;
+
+  history.pushState('', '', linkNextPage.href);
+  addEventListener("popstate", prevPage);
+
   requestAnimationFrame(function(){
-    elemActiveStiker.style.transition = 'all 2000ms ease';
-    elemActiveStiker.style.transform  = 'translateX(-50rem) translateY(-50%)';
-    elemActiveStiker.style.opacity    = 0;
+
+    elemActiveStiker.style.transition = 'all 1000ms ease';
+    elemActiveStiker.style.transform  = 'translateX('+ coordsPrevStiker.x +'px) translateY('+ coordsPrevStiker.y +'px)';
+
+    elemPrevStiker.style.transition = 'all 2000ms ease';
+    elemPrevStiker.style.transform  = 'translateX(-50rem) translateY(-50%)';
+    elemPrevStiker.style.opacity    = 0;
 
     elemNextStiker.style.transition = 'all 1500ms ease 500ms';
     elemNextStiker.style.transform  = 'translateX('+ coordsActiveStiker.x +'px) translateY('+ coordsActiveStiker.y +'px)';
@@ -54,8 +87,17 @@ function nextPage(event)
     setTimeout(function(){
       requestAnimationFrame(function() {
         elemNextStiker.querySelector('.stiker').style.transform = '';
-        elemArticle.innerHTML             = documentNextPage.querySelector('.case-detailed__article').innerHTML;
+
+        elemPrevStiker.querySelector('.stiker').innerHTML = elemActiveStiker.querySelector('.stiker').innerHTML;
+        elemPrevStiker.style.opacity   = '';
+        elemPrevStiker.style.transform = '';
+        elemPrevStiker.style.transition = '';
+
+        elemActiveStiker.style.transform  = 'translateX('+ coordsActiveStiker.x +'px) translateY('+ coordsActiveStiker.y +'px)';
         elemActiveStiker.style.transition = '';
+        elemActiveStiker.style.opacity    = 0;
+
+        elemArticle.innerHTML             = documentNextPage.querySelector('.case-detailed__article').innerHTML;
         elemArticle.style.transition      = '';
         elemArticle.style.transform       = 'translateX(50rem)';
         window.scrollTo(0, 0);
@@ -69,9 +111,8 @@ function nextPage(event)
 
     setTimeout(function(){
       requestAnimationFrame(function(){
-        elemActiveStiker.querySelector('.stiker').innerHTML       = elemNextStiker.querySelector('.stiker').innerHTML;
+        elemActiveStiker.querySelector('.stiker').innerHTML = elemNextStiker.querySelector('.stiker').innerHTML;
         elemActiveStiker.style.opacity   = '';
-        elemActiveStiker.style.transform = '';
 
         elemNextStiker.innerHTML        = documentNextPage.querySelector('.case-detailed__next').innerHTML;
         elemNextStiker.style.transition = '';
@@ -80,7 +121,6 @@ function nextPage(event)
         elemNextStiker.style.left       = '';
         elemNextStiker.style.transform  = 'translateX(50rem) translateY(-50%)';
 
-        history.pushState('', '', linkNextPage.href);
         document.title = documentNextPage.title;
         document.querySelector('.header__page-title').innerHTML = documentNextPage.querySelector('.header__page-title').innerHTML;
 
@@ -90,21 +130,32 @@ function nextPage(event)
           elemNextStiker.style.opacity    = 1;
           elemNextStiker.style.transform  = '';
 
-          timerClearStyle = setTimeout(function(){
-            requestAnimationFrame(function(){
-              elemSideRight.removeAttribute('style');
-              elemActiveStiker.removeAttribute('style');
-              elemNextStiker.removeAttribute('style');
-              elemArticle.removeAttribute('style');
+           timerClearStyle = setTimeout(function(){
+             requestAnimationFrame(function(){
+               elemSideRight.removeAttribute('style');
+               elemActiveStiker.removeAttribute('style');
+               elemNextStiker.removeAttribute('style');
+               elemPrevStiker.removeAttribute('style');
+               elemArticle.removeAttribute('style');
+             });
+           }, 2000);
 
-              documentNextPage = undefined;
-              for (var i = readyjs.length - 1; i >= 0; i--) {
-                readyjs[i]();
-              }
-            });
-          }, 2000);
+          document.documentElement.classList.remove('next-doc-ready');
+          document.documentElement.classList.remove('prev-doc-ready');
+          documentNextPage = undefined;
+          documentPrevPage = undefined;
+
+          for (var i = readyjs.length - 1; i >= 0; i--) {
+            readyjs[i]();
+          }
+
         });
       });
     }, 2000);
   });
+}
+
+
+function prevPage() {
+  alert('Скоро можно будет и назад листать! Вот прям щас работаем над этим. Пожалуйста не бейте программиста очень больно. Он очень старается научится все делать вовремя. Четсное слово! =)');
 }
